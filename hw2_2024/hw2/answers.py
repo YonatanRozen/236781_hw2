e120601691b69e60f1d92bd20328c019c6ca5397
YonatanRozen <102473544+YonatanRozen@users.noreply.github.com>
2024-07-26 14:04:35 +0300

    Finished open questions in part 3 & 5

--- hw2_2024/hw2/answers.py
+++ hw2_2024/hw2/answers.py
@@ -202,41 +202,60 @@ def part3_optim_hp():
 
 
 part3_q1 = r"""
-**Your answer:**
+
+1. Optimization error - refers to the difference between the model's predictions and the 
+actual values on the training data. This error is typically related to the model's ability to fit the 
+data and usually caused by bad initialization, optimization technique model architecture etc. 
+
+In CNNS, this error can be reduced by improving the model's architecture 
+and making sure the receptive field of each layer is sufficient for the model to learn
+important patterns in the data. This can increase the ability of the model to fit the data, and lower
+the optimization error. 
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+2. Generalization error - refers to the model's ability to correctly predict classes of examples which weren't 
+included in the training (to "generalize"). This error typically happens when the model is too biased about 
+the training data and over fits it, thus performing poorly on general data.
+
+A high generalization error can be reduced with regularization which involves penalizing complex models to reduce the
+ chance of over fitting, cross-validation which involves a pre-processing phase which picks hyperparameters based on 
+ approximated generalization error, and adjusting the receptive field of convolution layers to prevent irrelevant
+ features to be learned by them and cause over fitting.
+ 
+
+3. Approximation error - quantifies how much the model is suitable for fitting the data. A high approximation
+error typically means that the model is too simple to learn the relationships in the data. 
+
+It can be lowered by increasing model complexity and using a more advanced architecture. In CNNs specifically,
+we can expand the receptive field of each layer, add more layers, use more advanced architectures like residual 
+blocks and etc. 
+
+Generally, lowering each of these errors will consequently lower the population error. 
 
 """
 
 part3_q2 = r"""
 **Your answer:**
 
+A higher FPR makes more sense in cases in which classifying an example as positive by mistake (FP), causes less
+damage than missing an actually positive example (FN). A good example for this is a database of medical tests
+for corona virus for example. We prefer to diagnose someone as sick by mistake, than to diagnose a sick person
+as healthy and risking mass infection. 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
 part3_q3 = r"""
-**Your answer:**
+**Im assuming that "optimal point on the ROC curve" means a point where the threshold values for the classifier is optimal
+in terms ov FNR and FPR**
 
+1. If the disease causes symptoms which aren't lethal in early phases, our focus will be to reduce the FPR
+thus lowering the cost of unnecessary tests for patients who aren't sick. Later on, we can diagnose the disease
+on the false negatives when symptoms begin to appear, making sure they don't lose their lives. 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+2. If a person with the disease doesn't show any clear symptoms and may die in high probability if not 
+diagnosed, we prefer saving lives. This means we want a lower FNR, and accept a higher FPR in the goal
+of saving more lives and diagnose more people early. 
 
 """
 
@@ -244,13 +263,15 @@ An equation: $e^{i\pi} -1 = 0$
 part3_q4 = r"""
 **Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+The problem with using MLPs on sequential data is that MLPs treat each input they get independently and they 
+don't consider the context of the input and its relationship with other inputs. This makes them hard to use
+when trying to learn the sentiment of a sentence, which is a task that requires learning the context of each word
+in addition to the word itself. For example, say we want to learn the meaning of "it's not rainy today" 
+with an MLP. The MLP doesn't capture the effect that "not" has on the following word "rainy", which is negation, so 
+it might predict a wrong sentiment for this sentence. Also, MLPs require an input which is fixed in length, 
+and since the length of sentences and words vary, it will be hard to use a fixed input length for this task. We
+can break the sentence to fixed size parts, but this can result in fixed size parts which have no meaning 
+and contain half words, which changes the sentiment of the sentence.  
 
 """
 # ==============
@@ -308,54 +329,55 @@ part4_q1 = r"""
 
 
 part5_q1 = r"""
-**Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+    1. As we can see in the graphs, the test accuracy was lower as the depth increased and the depth that 
+    produced the best results was 2. For example, we can see that when L=4 we get
+    higher training accuracy than when L=2, but lower test accuracy. 
+    This can be caused by over fitting: deeper networks are more expressive
+    and learn more complex patterns in the data, however, they are more prone to learn irrelevant patterns and 
+    noise which can lead to poor generalization. 
+    
+    2. The network wasn't trainable for K=8,16. We assume the reason for this is the phenomenon of vanishing gradients,
+    which typically occurs when the number of layers is high. The high amount of layers causes gradients from the 
+    higher layers to "disappear" when performing backward propagation, turning the final gradients eventually to zero. 
+    This hurts th model's ability to learn and update its parameters based on the gradients. This problem can be
+    partially solved with incorporation of skip layers such as residual blocks. These can allow gradients to flow
+    directly to lower layers, without them vanishing and turning the whole gradient to zero.
+    
 """
 
 part5_q2 = r"""
-**Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+    Examination of the graphs brings up a few conclusions: 
+    1. For a fixed value of L=2 or L=4, bigger K values achieve higher training and test accuracies. This increase
+        in accuracy is much more clear than in experiment 1_1. 
+    2. For L=8 all values of K achieve very low accuracies. This supports our previous assumption in question 1.2.
+    3. The configuration that achieved the best results is L=2, K=128, in comparison to experiment 1_1 where the 
+    best configuration was L=2, K=64. 
+    So overall, we see a clear increase in accuracy as K grows (for L=2,4) and the best configuration was different than
+    in experiment 1_1.
 
 """
 
 part5_q3 = r"""
-**Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+    When examining the graphs, we can see that the model's performance varies for different depths. The depth
+    that achieved the highest accuracy is a moderate depth L=3, in contrast to previous experiments. Also, a depth of 2 
+    achieved higher train accuracy than a depth of 3 in almost all epochs, but lower test accuracy. This indicates 
+    that a depth of 3 enables the model to learn more complex patterns in the data and generalize better than with 
+    depth 2. Finally, we can see that when L=4 the model isn't trainable. This aligns with our previous assumption 
+    (since L=4 with K=[64,128] means there are are 8 conv layers) and could indicate the presence of vanishing gradients. 
+    
 """
 
 part5_q4 = r"""
-**Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+First of all, as in experiment 1.1 we can see that lower depths produced better test accuracies. This 
+only partially aligns with experiment 1.3 since the best depth in experiment 1.3 was moderate. Another 
+important observation is that when K is fixed to 32, the problem of vanishing gradients in higher depths
+is gone. This happens thanks to the residual blocks in resnet, which can help with this problem as we explained 
+in q1.2. Moreover, the vanishing gradients problem was dealt with success even when K was increased to 
+bigger sizes ([64,128,256]), and L=2 achieved the best test accuracies.   
 
 """
 
